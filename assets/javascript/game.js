157e03047c2a01fdf9f0415b25d4bea914ec36b0
trnkev <ktran775@yahoo.com>
2019-02-13 00:07:18 -0800

    images shown on page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,14 @@
 var wins = 0;
 var losses = 0;
-var score = 0;
-var guess = 0;
+var counter = 0;
+var targetNumber = Math.floor(Math.random()*100 + 25);
+var numberOptions = [10, 5, 3, 7];
+
+$('#number-to-guess').text(targetNumber);
+
+for (let picture of numberOptions) {
+    var crystalImage = $('<img>');
+    crystalImage.addClass('crystal-image');
+    crystalImage.attr('src', 'assets/images/crystal.svg');
+    $('#crystal').append(crystalImage);
+}
