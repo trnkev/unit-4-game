b11302bb783b13b87e316242e2db26e35897ff09
trnkev <ktran775@yahoo.com>
2019-02-14 04:32:17 -0800

    added loop to set value of crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,12 +3,14 @@ var losses = 0;
 var counter = 0;
 var targetNumber = Math.floor(Math.random()*100 + 25);
 var numberOptions = [10, 5, 3, 7];
+var imageOptions = ['gems','diamond','crystal','crystals'];
 
 $('#number-to-guess').text(targetNumber);
 
-// for (let picture of numberOptions) {
-//     var crystalImage = $('<img>');
-//     crystalImage.addClass('crystal-image');
-//     crystalImage.attr('src', 'assets/images/crystal.svg');
-//     $('#crystal').append(crystalImage);
-// }
+for (var i = 0; i < numberOptions.length; i++) {
+    var crystalImage = $('<img>');
+    crystalImage.addClass('crystal-image');
+    crystalImage.attr('src', 'assets/images/' + imageOptions[i] +'.svg');
+    crystalImage.attr('data-value', numberOptions[i]);
+    $('#crystal').append(crystalImage);
+}
